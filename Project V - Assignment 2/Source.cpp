c78899c346dcf279f3e613e5256e4ca22c2d2d42
MarcoPoloRolo <Marko03.cv@gmail.com>
2024-09-23 10:00:51 -0400

    Step 5: Creating a Pre-Release

--- Project V - Assignment 2/Source.cpp
+++ Project V - Assignment 2/Source.cpp
@@ -10,6 +10,7 @@ using namespace std;
 struct STUDENT_DATA {
     string firstName;
     string lastName;
+    string email;
 };
 
 int main() {
@@ -27,7 +28,7 @@ int main() {
 
     string line;
     //Read the file line by line
-    while (std::getline(inputFile, line)) {
+    while (getline(inputFile, line)) {
         stringstream ss(line);
         string firstName, lastName;
 
@@ -43,12 +44,41 @@ int main() {
 
     inputFile.close();
 
-    #ifdef _DEBUG
-    cout << "Debug Mode: Printing student data..." << endl;
-    //Display the students' data to verify it's stored correctly
-    for (const auto& student : students) {
-        cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << std::endl;
+    // Check for Pre-Release mode
+    #ifdef PRE_RELEASE
+    cout << "Pre-Release Mode: Loading student emails..." << endl;
+
+    // Open the StudentData_Emails.txt file
+    ifstream emailFile("StudentData_Emails.txt");
+
+    if (!emailFile.is_open()) {
+        cerr << "Error opening email file" << endl;
+        return 1;
     }
+
+    //Read the email file and update the student vector with emails
+    int index = 0;
+    while (getline(emailFile, line)) {
+        stringstream ss(line);
+        string firstName, lastName, email;
+
+        if (getline(ss, firstName, ',') && getline(ss, lastName, ',') && getline(ss, email)) {
+            if (index < students.size() && students[index].firstName == firstName && students[index].lastName == lastName) {
+                students[index].email = email; // Add the email to the student
+            }
+            index++;
+        }
+    }
+
+    emailFile.close();
+
+    //Display students with emails
+    for (const auto& student : students) {
+        cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << ", Email: " << student.email << endl;
+    }
+
+    #else
+    cout << "Standard Mode: No emails loaded." << endl;
     #endif
 
     return 0;
